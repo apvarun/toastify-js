11754a097fb662dbcf7639b6eeac470590816131
acaiberii <69873653+AcaiBerii@users.noreply.github.com>
2021-10-05 16:51:34 -0400

    Add support for custom styles for on and off.
    (shh shh just renaming toast-close to be fluent with the now renamed toast-on)

--- src/toastify.js
+++ src/toastify.js
@@ -99,7 +99,7 @@
 
       // Creating the DOM object
       var divElement = document.createElement("div");
-      divElement.className = "toastify on " + this.options.className;
+      divElement.className = "toast-on " + this.options.className;
 
       // Positioning toast to left or right or center
       if (!!this.options.position) {
@@ -161,7 +161,7 @@
         var closeElement = document.createElement("span");
         closeElement.innerHTML = "&#10006;";
 
-        closeElement.className = "toast-close";
+        closeElement.className = "toast-off";
 
         // Triggering the removal of toast from DOM on close click
         closeElement.addEventListener(
@@ -306,7 +306,7 @@
     removeElement: function(toastElement) {
       // Hiding the element
       // toastElement.classList.remove("on");
-      toastElement.className = toastElement.className.replace(" on", " off");
+      toastElement.className = toastElement.className.replace("-on", "");
 
       // Removing the element from DOM after transition end
       window.setTimeout(
@@ -327,7 +327,7 @@
           // Repositioning the toasts again
           Toastify.reposition();
         }.bind(this),
-        500
+        400
       ); // Binding `this` for function invocation
     },
   };
