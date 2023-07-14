e37c73c47445748ae0c6056e47f8e9669859a0a6
akii09 <akashpise588@gmail.com>
2023-07-15 03:40:35 +0530

    Feature - Sound functionality functions init

--- example/script.js
+++ example/script.js
@@ -1,41 +1,41 @@
 var bgColors = [
-    "linear-gradient(to right, #00b09b, #96c93d)",
-    "linear-gradient(to right, #ff5f6d, #ffc371)",
-  ],
-  i = 0;
+  "linear-gradient(to right, #00b09b, #96c93d)",
+  "linear-gradient(to right, #ff5f6d, #ffc371)",
+],
+i = 0;
 
 Toastify({
-  text: "Hi",
-  duration: 4500,
-  destination: "https://github.com/apvarun/toastify-js",
-  newWindow: true,
-  gravity: "top",
-  position: 'left',
+text: "Hi",
+duration: 4500,
+destination: "https://github.com/apvarun/toastify-js",
+newWindow: true,
+gravity: "top",
+position: 'left',
 }).showToast();
 
 setTimeout(function() {
-  Toastify({
-    text: "Simple JavaScript Toasts",
-    gravity: "top",
-    position: 'center',
-    style: {
-      background: '#0f3443'
-    }
-  }).showToast();
+Toastify({
+  text: "Simple JavaScript Toasts",
+  gravity: "top",
+  position: 'center',
+  style: {
+    background: '#0f3443'
+  }
+}).showToast();
 }, 1000);
 
 // Options for the toast
 var options = {
-  text: "Happy toasting!",
-  duration: 2500,
-  callback: function() {
-    console.log("Toast hidden");
-    Toastify.reposition();
-  },
-  close: true,
-  style: {
-    background: "linear-gradient(to right, #00b09b, #96c93d)",
-  }
+text: "Happy toasting!",
+duration: 2500,
+callback: function() {
+  console.log("Toast hidden");
+  Toastify.reposition();
+},
+close: true,
+style: {
+  background: "linear-gradient(to right, #00b09b, #96c93d)",
+}
 };
 
 // Initializing the toast
@@ -43,30 +43,48 @@ var myToast = Toastify(options);
 
 // Toast after delay
 setTimeout(function() {
-  myToast.showToast();
+myToast.showToast();
 }, 4500);
 
 setTimeout(function() {
-  Toastify({
-    text: "Highly customizable",
-    gravity: "bottom",
-    position: 'left',
-    close: true,
-    style: {
-      background: "linear-gradient(to right, #ff5f6d, #ffc371)",
-    }
-  }).showToast();
+Toastify({
+  text: "Highly customizable",
+  gravity: "bottom",
+  position: 'left',
+  close: true,
+  style: {
+    background: "linear-gradient(to right, #ff5f6d, #ffc371)",
+  }
+}).showToast();
 }, 3000);
 
 // Displaying toast on manual action `Try`
 document.getElementById("new-toast").addEventListener("click", function() {
-  Toastify({
-    text: "I am a toast",
-    duration: 3000,
-    close: i % 3 ? true : false,
-    style: {
-      background: bgColors[i % 2],
-    }
-  }).showToast();
-  i++;
+Toastify({
+  text: "I am a toast",
+  duration: 3000,
+  close: i % 3 ? true : false,
+  style: {
+    background: bgColors[i % 2],
+  }
+}).showToast();
+i++;
+});
+
+// Displaying toast on manual action `Try Sound`
+document.getElementById("new-toast-sound").addEventListener("click", function() {
+Toastify({
+  text: "I am a toast",
+  duration: 3000,
+  sound: {
+    default: false,
+    type: 'bell',
+    src: './sounds/alert.wav' // If having custom file
+  },
+  close: i % 3 ? true : false,
+  style: {
+    background: bgColors[i % 2],
+  }
+}).showToast();
+i++;
 });
